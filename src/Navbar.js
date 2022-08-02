47f1337f56d44cfbb2b0bb050550aa36c426180f
Nicholas Ao <aominhlong@yahoo.com>
2022-08-02 15:46:38 -0600

    Add input field and styling and save state of user input

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,9 +2,17 @@ import React, { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
+    const [text, setText] = useState('')
+
     return (
-        <div>
+        <div className='navbar'>
+            <input 
+            value={ text }
+            placeholder='search'
+            onChange={(event) => setText(event.target.value)}
+            >
             
+            </input>
         </div>
     )
 }

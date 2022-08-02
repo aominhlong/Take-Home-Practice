3fc677f01958134494bf94a531475dc56bfbc60c
Nicholas Ao <aominhlong@yahoo.com>
2022-08-02 14:32:45 -0600

    Add basic components and successfully fetch articles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import Navbar from './Navbar';
+import ArticlesContainer from './ArticlesContainer';
 
-function App() {
+const App = () => {
+
+  const [allData, setAllData] = useState([])
+  const [filteredData, setFilteredData] = useState({})
+
+  useEffect(() => {
+      fetch('https://api.nytimes.com/svc/topstories/v2/us.json?api-key=zFAFLWv8eNjlWC8oPAFmHbme0aAvA6RZ')
+      .then(res => res.json())
+      .then(data => {
+        setAllData(data)
+      })
+  }, [])
+
+  console.log('hi', allData)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ArticlesContainer data={ allData } />
+      
     </div>
   );
 }
---
+++ src/ArticlesContainer.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import ArticleDetails from './ArticleDetails';
+import './ArticlesContainer.css'
+
+const ArticlesContainer = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ArticlesContainer;

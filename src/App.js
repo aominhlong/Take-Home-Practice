5434d0b2f15583721b27cdd7103aa6a40584fa5b
Nicholas Ao <aominhlong@yahoo.com>
2022-08-02 15:07:06 -0600

    Render articles on landing page

--- src/App.js
+++ src/App.js
@@ -7,20 +7,22 @@ const App = () => {
 
   const [allData, setAllData] = useState([])
   const [filteredData, setFilteredData] = useState({})
+  const [genre, setGenre] = useState('world')
 
   useEffect(() => {
-      fetch('https://api.nytimes.com/svc/topstories/v2/us.json?api-key=zFAFLWv8eNjlWC8oPAFmHbme0aAvA6RZ')
+      fetch(`https://api.nytimes.com/svc/topstories/v2/${genre}.json?api-key=zFAFLWv8eNjlWC8oPAFmHbme0aAvA6RZ`)
       .then(res => res.json())
       .then(data => {
-        setAllData(data)
+        setAllData(data.results)
       })
   }, [])
 
   console.log('hi', allData)
   return (
     <div className="App">
+      <h1>NY Times Top Stories</h1>
+      <Navbar />
       <ArticlesContainer data={ allData } />
-      
     </div>
   );
 }
--- src/ArticlesContainer.js
+++ src/ArticlesContainer.js
@@ -2,10 +2,17 @@ import React, { useState } from 'react';
 import ArticleDetails from './ArticleDetails';
 import './ArticlesContainer.css'
 
-const ArticlesContainer = () => {
+const ArticlesContainer = ({ data }) => {
+    console.log(data)
+    const articles = data.map(article => {
+        return <div className='article'>
+            <h3>{article.title}</h3>
+            <p>{article.abstract}</p>
+        </div>
+    })
     return (
         <div>
-
+            { articles }
         </div>
     )
 }

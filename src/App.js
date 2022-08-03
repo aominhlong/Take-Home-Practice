3321e39a9d0bbe3c49527f0f17745d58ccd9a0e4
Nicholas Ao <aominhlong@yahoo.com>
2022-08-03 09:07:35 -0600

    Add router and search bar functionality

--- src/App.js
+++ src/App.js
@@ -2,29 +2,41 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import Navbar from './Navbar';
 import ArticlesContainer from './ArticlesContainer';
+import { Route } from 'react-router-dom'
 
 const App = () => {
 
   const [allData, setAllData] = useState([])
-  const [filteredData, setFilteredData] = useState({})
+  const [filteredData, setFilteredData] = useState([])
   const [genre, setGenre] = useState('world')
+  const [text, setText] = useState('')
 
   useEffect(() => {
       fetch(`https://api.nytimes.com/svc/topstories/v2/${genre}.json?api-key=zFAFLWv8eNjlWC8oPAFmHbme0aAvA6RZ`)
       .then(res => res.json())
       .then(data => {
         setAllData(data.results)
+        setFilteredData(data.results)
       })
   }, [])
 
-  console.log('hi', allData)
-  console.log(genre)
+  const searchArticles = (keyword) => {
+    console.log(filteredData.length)
+    const filteredList = allData.filter(data => {
+      return data.abstract.includes(keyword) 
+    })
+    setFilteredData(filteredList)
+  }
+  
   return (
     <div className="App">
       <h1 className='title'>NY Times Top Stories</h1>
-      <Navbar />
-      <h2>Currently looking at: {genre.charAt(0).toUpperCase()+ genre.slice(1)} News</h2>
-      <ArticlesContainer data={ allData } />
+      <Navbar setFilteredData={ setFilteredData } searchArticles={ searchArticles } text={text} setText={setText}/>
+      <h2>Currently looking at: { genre.charAt(0).toUpperCase()+ genre.slice(1) } News</h2>
+      <Route exact path="/" render={() => {
+        <ArticlesContainer data={ allData } />
+      }}></Route>
+      { filteredData.length >= 1 && <ArticlesContainer data={ filteredData } /> }
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 import './Navbar.css';
 
-const Navbar = () => {
-    const [text, setText] = useState('')
+const Navbar = ({ searchArticles, setFilteredData, text, setText }) => {
+    // const [text, setText] = useState('')
 
     return (
         <div className='navbar'>
             <input 
             value={ text }
             placeholder='search'
-            onChange={(event) => setText(event.target.value)}
+            onChange={(event) => {
+                setText(event.target.value)
+                searchArticles(text)
+            }}
             >
-            
             </input>
+            <button className='clear-search' onClick={() => setText('')}>clear</button>
         </div>
     )
 }

153e60ac5052201b4f380ba6dee1bacc40a22e43
Nicholas Ao <aominhlong@yahoo.com>
2022-08-04 15:44:18 -0600

    Add home button

--- src/ArticleDetails.js
+++ src/ArticleDetails.js
@@ -10,16 +10,14 @@ const ArticleDetails = ({ specificArticle }) => {
 
     const articleDate = specificArticle.published_date.split('T').splice(0,1)
 
-    console.log(articleDate)
-
     return (
         <div>
             <h1>{ specificArticle.title }</h1>
             <img className='article-img' src={ specificArticle.multimedia[0].url}></img>
             <br></br>
             <p>{ specificArticle.abstract }</p>
-            <p className='view-article-link'>View full article <span className='full-article' onClick={() => addUrl(specificArticle.url)}>here</span></p>
             <p>Published: { articleDate[0] }</p>
+            <h4 className='view-article-link'>View full article <span className='full-article' onClick={() => addUrl(specificArticle.url)}>here</span></h4>
         </div>
     )
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ searchArticles, setFilteredData, text, setText }) => {
     // const [text, setText] = useState('')
 
     return (
         <div className='navbar'>
+            <Link to='/'>
+                <button className='home-btn'>Home</button>
+            </Link>
             <input 
             value={ text }
             placeholder='search'
